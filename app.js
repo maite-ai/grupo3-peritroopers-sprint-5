d2f6611a3099237865d9474885a2fa0e408e2fef
matiasbenavide <matiasbenavide@gmail.com>
2021-05-22 18:18:02 -0300

    Yisus login usuario

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 const port = process.env.PORT;
 const path = require("path");
 const methodOverride = require("method-override");
+const session = require('express-session')
+const cookies=require('cookie-parser')
+const userLoggedMiddleware=require('./middlewares/userLoggedMiddleware')
 
 const publicPatch = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPatch));
-
+app.use(session({
+    secret:"Secret",
+    resave:false,
+    saveUinitialized:false
+}))
+app.use(cookies())
+app.use(userLoggedMiddleware)
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
@@ -24,6 +33,6 @@ app.use((req, res, next) => {
     res.status(404).render('error404');                                         
 });
 
-app.listen(port || 3030, () => {
+app.listen(port || 3040, () => {
     console.log("El servidor está corriendo con éxito");
 });
